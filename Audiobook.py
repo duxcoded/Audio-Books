efda95c7febbd559341f811d6d45a9e7b1a3cc9c
Dunx <80205877+duxcoded@users.noreply.github.com>
2021-06-29 19:38:39 +0700

    Initial commit

---
+++ Audiobook.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 08:55:51 2021
+AudioBooks
+@author: duxcode
+"""
+import pyttsx3
+import PyPDF2
+
+
+#Mở audio book. PDF dịnh dạng là binary nên ta dùng rb = read binary
+book =  open("se.pdf", "rb")
+
+#Goi thư viện PDF2 để đoc file PDF
+pdfreader = PyPDF2.PdfFileReader(book)
+
+#Đếm số trang của file
+pages = pdfreader.numPages
+print(pages)
+
+#Setup pyttsx3 cơ bản
+##Tạo object bot
+bot = pyttsx3.init()
+##Tạo object voices và lấy tất cả giọng nói trong máy ra
+voices = bot.getProperty("voices")
+##Chọn giọng cho voices: 0 nam; 1 nữ
+bot.setProperty("voice", voices[1].id)
+##Đọc từng trang cụ thể
+# page = pdfreader.getPage(9)
+#L#ấy text từ file pdf để đọc
+# text = page.extractText()
+# bot.say(text)
+# bot.runAndWait()
+
+
+#Đọc từ rang 8 đến hết sách. Ta tạo object pages ở trên để làm việc này
+for num in range(106, pages):
+    page = pdfreader.getPage(num)
+    text = page.extractText()
+    bot.say(text)
+    bot.runAndWait()
